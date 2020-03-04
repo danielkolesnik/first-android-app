f5b9b8108732afa3eb681578660c8fc32c2164a0
dkolesnik <daneelkolesnik@gmail.com>
2020-03-04 20:41:02 +0200

    [*] color scheme changed; [+] implemented date picker fragment; [+] implemented fields validation before redirecting; [+] implemented "lil' jokes" flow for second activity (for now there are 3 options of age-dependent messages available; <18, <40 and >40 will receive different "lil' joke";

--- app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.firstandroidapp;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private static final String TAG = "@@MainActivity";
 
     public static final String USR_NAME = "com.example.firstandroidapp.USR_NAME";
     public static final String USR_SURNAME = "com.example.firstandroidapp.USR_SURNAME";
+    public static final String USR_BIRTH_DATE = "com.example.firstandroidapp.USR_BIRTHDATE";
+    public static final String USR_BIRTH_YEAR = "com.example.firstandroidapp.USR_BIRTHYEAR";
+
+    private String usrYearOfBirth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +32,70 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Handle attempt to go to the next activity
+     * @param view
+     */
     public void letsGoBtnClickHandler(View view) {
-        // React
+
+        String usrName = ((EditText) findViewById(R.id.vetName)).getText().toString();
+        String usrSurname = ((EditText) findViewById(R.id.vetSurname)).getText().toString();
+        String usrBirthDate = ((EditText) findViewById(R.id.vetBirthDate)).getText().toString();
+        Toast emptyValueToast;
+
+        if (usrName.isEmpty()) {
+            emptyValueToast = Toast.makeText(getApplicationContext(), "Name is required, dummy..\n(as all the other fields)", Toast.LENGTH_SHORT);
+            emptyValueToast.setGravity(Gravity.TOP, 0, 200);
+            emptyValueToast.show();
+            return;
+        }
+        if (usrSurname.isEmpty()) {
+            emptyValueToast = Toast.makeText(getApplicationContext(), "Surname is required, dummy..\n(as all the other fields)", Toast.LENGTH_SHORT);
+            emptyValueToast.setGravity(Gravity.TOP, 0, 200);
+            emptyValueToast.show();
+            return;
+        }
+        if (usrBirthDate.isEmpty()) {
+            emptyValueToast = Toast.makeText(getApplicationContext(), "Birth date is required, dummy..\n(as all the other fields)", Toast.LENGTH_SHORT);
+            emptyValueToast.setGravity(Gravity.TOP, 0, 200);
+            emptyValueToast.show();
+            return;
+        }
 
         Intent intent = new Intent(this, SecondActivity.class);
 
-        String usrName = ((EditText) findViewById(R.id.nameEditText)).getText().toString();
-        String usrSurname = ((EditText) findViewById(R.id.surnameEditText)).getText().toString();
-
-        // add validation of user credentials validation
-
+        // add user credentials validation
+        Log.i(TAG, "usrName: \t\t" + usrName +
+                "\nusrSurname: \t\t" + usrSurname +
+                "\nusrBirthDate: \t\t" + usrBirthDate +
+                "\nthis.usrYearOfBirth: \t" + usrYearOfBirth
+        );
         intent.putExtra(USR_NAME, usrName);
         intent.putExtra(USR_SURNAME, usrSurname);
+        intent.putExtra(USR_BIRTH_DATE, usrBirthDate);
+        intent.putExtra(USR_BIRTH_YEAR, usrYearOfBirth);
         startActivity(intent);
+    }
+
+    /**
+     * Open DatePicker to select User BirthDate
+     *
+     * @param view
+     */
+    public void vetBirthDateClickHandler(View view) {
+        DialogFragment datePicker = new DatePickerFragmentDialog();
+        datePicker.show(getSupportFragmentManager(), "Custom Date Picker");
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+
+        // set full date to edit text view
+        ((EditText) findViewById(R.id.vetBirthDate)).setText(String.format("%d/%d/%d", dayOfMonth, month, year));
+
+        // also set usrYearOfBirth field to pass it further into Second Activity without parsing full dateOfBirth
+        usrYearOfBirth = "" + year;
+
     }
 }
--- app/src/main/java/com/example/firstandroidapp/SecondActivity.java
+++ app/src/main/java/com/example/firstandroidapp/SecondActivity.java
@@ -2,11 +2,16 @@ package com.example.firstandroidapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "@@SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +19,24 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         Intent intent = getIntent();
-        String welcomeMessage = "Dear " + intent.getStringExtra(MainActivity.USR_NAME) +
-                intent.getStringExtra(MainActivity.USR_SURNAME) + ", Welcome to The Best App Ever!!!";
+        String welcomeMessage = "dear " + intent.getStringExtra(MainActivity.USR_NAME) + " " +
+                intent.getStringExtra(MainActivity.USR_SURNAME);
 
-        ((TextView) findViewById(R.id.welcomeTextView)).setText(welcomeMessage);
+        String lilJokeMessage = "";
+        int usrAge = (Calendar.getInstance().get(Calendar.YEAR)) - Integer.parseInt(intent.getStringExtra(MainActivity.USR_BIRTH_YEAR));
+
+        Log.w(TAG, "usrAge: \t\t" + usrAge
+        );
+
+        if (usrAge < 18) {
+            lilJokeMessage = "too young to visit such apps!! lil' bastard..";
+        } else if (usrAge < 40) {
+            lilJokeMessage = "exactly in our target auditory \n(you and the other billion of people...)";
+        } else {
+            lilJokeMessage = "goin' home dirty pensioner, lol";
+        }
+
+        ((TextView) findViewById(R.id.vtUsrName)).setText(welcomeMessage);
+        ((TextView) findViewById(R.id.vtLilJoke)).setText(lilJokeMessage);
     }
 }
